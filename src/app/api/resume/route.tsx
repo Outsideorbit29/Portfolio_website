eb312f39ea4a8109296dfc986bd84535ca57eb55
Outsideorbit29 <anish.grd2004@gmail.com>
2026-07-04 16:11:20 +0530

    Update resume download link with new Google Drive file ID

--- src/app/api/resume/route.tsx
+++ src/app/api/resume/route.tsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     // Direct Google Drive download link for the user's resume
-    const resumeUrl = "https://drive.google.com/uc?export=download&id=1WFvry6SmsLK05WcYinBT5_iNbaK1lI5C";
-    
+    const resumeUrl = "https://drive.google.com/file/d/1zDAR4YAHg3FGSX-f-MyKW-sDdjJjGyh9/view?usp=sharing";
+
     return NextResponse.redirect(resumeUrl);
 }
