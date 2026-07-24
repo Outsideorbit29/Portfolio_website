86f79196918a1cc5a3ec9110a43bf0e50d05e236
Outsideorbit29 <anish.grd2004@gmail.com>
2026-07-24 13:02:06 +0530

    Update resume drive link again

--- src/app/api/resume/route.tsx
+++ src/app/api/resume/route.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export const dynamic = 'force-dynamic';
 export async function GET() {
     // Direct Google Drive download link for the user's resume
-    const resumeUrl = "https://drive.google.com/uc?export=download&id=1sQum4GVLRUq-wTjFAmY3eKEkajrCWIbc";
+    const resumeUrl = "https://drive.google.com/uc?export=download&id=1ErZDiBsC6mixD5PZf58YA3QBzZxfADf3";
 
     return NextResponse.redirect(resumeUrl);
 }
